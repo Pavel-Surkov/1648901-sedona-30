c243096f62241ba56635507283b8658532d1a315
Павел <psurkoff@mail.ru>
2021-02-22 17:15:05 +0700

    ТЗ 3.17. Добавляет отправку формы при нажатии кнопки ПОКАЗАТЬ. ТЗ 4.2. Добавлена 'анимация выезда' формы относительно кнопки. Телефон в футере сделан ссылкой.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ let times = 0;
 function showHide() {
 	let form = document.querySelector('.search-window');
 	times++;
-	if(times % 2 != 0) 
-		form.classList.add('hidden');
-	else 
-		form.classList.remove('hidden');
+	if(times % 2 != 0) {
+		form.classList.remove('form-down');
+		form.classList.add('form-up')
+	}
+	else {
+		form.classList.remove('form-up')
+		form.classList.add('form-down');
+	}
+
 }
